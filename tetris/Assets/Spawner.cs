db2e78b22a3793b39a38cca51453e022150df0e8
Gleb <glebsd@mail.ru>
2020-12-24 00:52:19 +0300

    game

---
+++ tetris/Assets/Spawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] Tetraminoes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NewTetramino();
+    }
+
+    public void NewTetramino()
+    {
+        Instantiate(Tetraminoes[Random.Range(0, Tetraminoes.Length)], transform.position, Quaternion.identity);
+    }
+}
